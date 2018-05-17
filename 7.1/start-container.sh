9e67d130f401c22f0c9775c16ab3b90e2c01d51c
Braydee Johnson <braydee.johnson@gmail.com>
2018-05-17 11:49:54 -0700

    Docker 7.1 Build Files

---
+++ 7.1/start-container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update Xdebug host
+sed -i "s|xdebug.remote_host.*|xdebug.remote_host = ${XDEBUG_REMOTE_HOST}|" /etc/php/7.1/mods-available/xdebug.ini
+sed -i "s|xdebug.idekey.*|xdebug.idekey = ${XDEBUG_REMOTE_KEY}|" /etc/php/7.1/mods-available/xdebug.ini
+sed -i "s|xdebug.remote_port.*|xdebug.remote_port = ${XDEBUG_REMOTE_PORT}|" /etc/php/7.1/mods-available/xdebug.ini
+
+#Run supervisord in Foreground
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
