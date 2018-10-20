b4287ba7fec64bb6b7d277b0593cb3540e06ad7f
Braydee Johnson <braydee.johnson@gmail.com>
2018-10-19 17:20:55 -0700

    Added config ENV support for VIRTUAL_HOST server_name and added lets encrypt well-known acme blocks.

--- 7.1/start-container.sh
+++ 7.1/start-container.sh
@@ -5,5 +5,8 @@ sed -i "s|xdebug.remote_host.*|xdebug.remote_host = ${XDEBUG_REMOTE_HOST}|" /etc
 sed -i "s|xdebug.idekey.*|xdebug.idekey = ${XDEBUG_REMOTE_KEY}|" /etc/php/7.1/mods-available/xdebug.ini
 sed -i "s|xdebug.remote_port.*|xdebug.remote_port = ${XDEBUG_REMOTE_PORT}|" /etc/php/7.1/mods-available/xdebug.ini
 
+# Update Nginx conf
+sed -i "s|server_name.*|server_name ${VIRTUAL_HOST};|" /etc/nginx/sites-available/laravel
+
 #Run supervisord in Foreground
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
